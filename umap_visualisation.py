27a3fcc57c25f363c8eb9ca68b6363debed04919
sarrahroseml <133075789+sarrahroseml@users.noreply.github.com>
2023-05-30 22:26:19 +0800

    Update umap_visualisation.py
    How to set up dependencies properly

--- umap_visualisation.py
+++ umap_visualisation.py
@@ -1,6 +1,12 @@
 !pip install umap-learn
-import umap
+import umap.umap_ as umap
+import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_harabasz_score
+
+#RawCT
+df_umap = pd.read_csv("/Users/sarrahrose/Downloads/29May_RawCT - Sheet1 (1).csv")
+X = df_umap[[ 'Biomarker 1','Biomarker 2','Biomarker 3','Biomarker 5','Biomarker 9','Biomarker 10','Biomarker 12','Biomarker 16']]  
 
 # Perform dimensionality reduction with UMAP
 umap_model = umap.UMAP(n_neighbors=20, min_dist=0.50, n_components=4, metric='manhattan',random_state=42)
@@ -23,4 +29,3 @@ calinski_harabasz_index = calinski_harabasz_score(umap_result, df_umap['Disease'
 print("Silhouette Coefficient: ", silhouette_coefficient)
 print("Davies-Bouldin Index: ", davies_bouldin_index)
 print("Calinski-Harabasz Index: ", calinski_harabasz_index)
-
